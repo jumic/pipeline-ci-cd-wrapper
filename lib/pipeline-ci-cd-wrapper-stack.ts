1235972825e4acf7e906cd1fce780406ac295ca5
Julian Michel <mail@julianmichel.de>
2025-03-14 21:55:59 +0100

    feat: init origin

--- lib/pipeline-ci-cd-wrapper-stack.ts
+++ lib/pipeline-ci-cd-wrapper-stack.ts
@@ -6,6 +6,8 @@ export class PipelineCiCdWrapperStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    console.log(process.env.RES_ACCOUNT_AWS_PROFILE)
+
     // The code that defines your stack goes here
 
     // example resource
